04665368b2d0a9f76b28742e84e4f4982be5d624
cecil <cepersico.3@gmail.com>
2023-07-02 18:59:18 +0200

    git remote add origin https://github.com/CePersico/JavaBasic.git git branch -M main git push -u origin main

---
+++ src/com/chechu/Seleccion.java
@@ -0,0 +1,90 @@
+package com.chechu;
+
+
+
+//Para comentar en Java en IntelliJ IDEA, se pueden utilizar los siguientes atajos de teclado12:
+// Comentario de línea: Ctrl + / (el cursor debe estar en la línea que se desea comentar)
+//        Comentario de bloque: Ctrl + Shift + / (seleccionar el bloque de código que se desea comentar)
+//        Eliminar una línea: Ctrl + y
+//        Duplicar una línea: Ctrl + d
+
+
+// las clases pueden ser: PUBLIC( le da cierta viscivilidad a la clase), PRIVATE O PROTECTED
+
+//import java.util.ArrayList;
+//public class Seleccion { // por convencion el nombrñe de una clase comineza con mayuscula y singular!!
+    // tipos de datos primitivos:
+    // int, byte, short, long, float, double, boolean, char
+
+    // Documentacion Oficial:
+    // Clases y metodos: https://docs.oracle.com/javase/8/docs/api/allclasses-noframe.html
+
+  //  String nombreEquipo;
+  //  char grupo;
+  //  boolean clasificadoACuartos;
+
+  //  void imprimirEquipo() {
+  //      System.out.println(nombreEquipo);
+  //      System.out.println(grupo);
+  //      System.out.println(clasificadoACuartos);
+  //  }
+
+    // la clase seleccion sera un objeto con la caracteristica de: grupo, nombre y si clasifico.
+  //  public static void main(String[] args) { // punto de entrada para el programa, escribiendo main sale tutti!!
+
+    //    int numeroDeEquipos = 32;
+    //    Seleccion argentina = new Seleccion();  // aca se crea un nuevo objeto con la clase seleccion.
+    //    argentina.nombreEquipo = "Argentina"; // aca accedemos a cargar las caracteristicas del objeto creado.
+    //    argentina.grupo = 'C';
+    //    argentina.clasificadoACuartos = true;
+
+        //argentina.imprimirEquipo();
+
+        //System.out.println(argentina.nombreEquipo);// escribir sout!!
+
+        // IF
+        //if(argentina.clasificadoACuartos == true) System.out.println("Clasificados");
+        //else if(argentina.clasificadoACuartos == false) System.out.println("Somos mailisimos");
+
+        // FOR
+
+        //for (int i = 1; i < 11; i++) {
+        //    System.out.println("Asiento nª " + i);
+        //}
+
+        // WHILE
+        //int j = 1;
+        //while(j < 11) {
+        //    System.out.println("Asiento nª " + j);
+        //    j++;
+        //}
+
+        // Excepciones: statement que interrumpe el programa
+
+    //    int x = 0, z = 5;
+        //System.out.println(z/x); // Exception in thread "main" java.lang.ArithmeticException: / by zero
+        //at com.chechu.Seleccion.main(Seleccion.java:60)
+
+        // veamos una forma de salvar ese problema con un try-catch:
+
+        //try {
+        //    System.out.println(z/x);
+        //} catch (Exception e) {
+        //    System.out.println("No se puede dividir por CERO");
+       //}
+
+        //API
+      //  ArrayList<String> Selecciones = new ArrayList<>();
+      //  Selecciones.add("Francia");
+      //  Selecciones.add("Inglaterra");
+
+      //  Selecciones.add("Brasil");
+      //  Selecciones.add("Marruecos");
+
+      //  System.out.println(Selecciones); // [Francia, Inglaterra, Brasil, Marruecos]
+
+
+    //}
+
+//}
+
